098aa2b88952ed87b8609afa6a63692c6eed02ea
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-20 13:28:00 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,36 @@
+export const item = {
+    "id": "0c6d864a-8eea-4172-ace4-3330a65fb364",
+    "title": "bill",
+    "categories_list": [
+        {
+            "id": "ed1c2c32-e5dd-4e0f-be5b-63cf77cf0a3b",
+            "category_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png",
+            "category_title": "raise",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png"
+        },
+        {
+            "id": "d5fd61d9-1e92-430d-8186-74c1621ada99",
+            "category_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png",
+            "category_title": "treatment",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+        },
+        {
+            "id": "87712682-a53a-4c1a-b2dd-e1ab488482ef",
+            "category_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png",
+            "category_title": "lot",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+        },
+        {
+            "id": "6cdb3ae3-bebb-4e7a-bbd1-00deb9e0478d",
+            "category_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png",
+            "category_title": "seat",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+        },
+        {
+            "id": "604cda44-5bd7-413e-b072-b9cc801e3f1a",
+            "category_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png",
+            "category_title": "walk",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+        }
+    ]
+}
